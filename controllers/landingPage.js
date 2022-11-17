1abfffd74950bd669b47231fadb1b43d7b09fb2c
aleksyara <aleksei.yarantsev@gmail.com>
2022-11-16 19:59:45 -0800

    refactor controllers menues, updated view menu and render menuItems in the tables, implemented Partisla fotter and header and added to the main menu page

--- controllers/landingPage.js
+++ controllers/landingPage.js
@@ -6,12 +6,7 @@ module.exports = {
 };
 
 function show(req, res) {
-// res.render("menu/show", {
-   res.render("index", {
+  res.render("index", {
     taco: Menu.getAll(),
-    // // Would like to display the number of the todo within the list
-    // todoNum:
-    //   Menu.getAll().findIndex((menu) => menu.id === parseInt(req.params.id)) +
-    //   1,
   });
 }
